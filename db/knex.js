aaa0b39f266105ba2de63ed7cd1135df5978e420
sircatalyst <temibami@gmail.com>
2019-06-05 13:57:36 +0100

    seeded the table with sample data

--- db/knex.js
+++
@@ -1,9 +0,0 @@
-var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile.js')[environment];
-var knex = require('knex')(config);
-
-const sql = knex('knex').toString();
-
-console.log(sql);
-
-knex.destroy();
---
+++ seedSample.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        title: "Javascript",
+        description: "JS Logo",
+        rating: 10,
+        url: "http://devstickers.com/assets/img/pro"
+    },
+    {
+        title: "ES6",
+        description: "ES6 Logo",
+        rating: 10,
+        url: "http://devstickers.com/assets/img/pro"
+    },
+    {
+        title: "Rainbow Javascript",
+        description: "JS Logo with Rainbow",
+        rating: 7,
+        url: "http://devstickers.com/assets/img/pro"
+    },
+    {
+        title: "Javascript Beer",
+        description: "JS Logo",
+        rating: 1,
+        url: "http://devstickers.com/assets/img/pro"
+    },
+    {
+        title: "Node Js",
+        description: "NodeJs Logo",
+        rating: 4,
+        url: "http://devstickers.com/assets/img/pro"
+    },
+    {
+        title: "Javascript Hexagon",
+        description: "JS Logo",
+        rating: 8,
+        url: "http://devstickers.com/assets/img/pro"
+    }
+]
